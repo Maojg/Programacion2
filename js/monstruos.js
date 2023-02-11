d4e3c2109da9929ad08d0144bc5df2344eccd293
Maojg <mauriciojaramillog@gmail.com>
2023-02-11 12:29:15 -0500

    24

--- js/monstruos.js
+++ js/monstruos.js
@@ -1,6 +1,16 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego() {
     let botonMonstruoJugador = document.getElementById('boton-monstruo')
     botonMonstruoJugador.addEventListener('click', seleccionarMonstruoJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let BotonAgua =  document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let BotonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
 }
 
 function seleccionarMonstruoJugador() {
@@ -18,6 +28,51 @@ function seleccionarMonstruoJugador() {
     }else {
         alert('Selecciona un Monstruo')    
     } 
+    seleccionarMonstruoEnemigo()
+}
+
+function seleccionarMonstruoEnemigo(){
+    let monstruoAleatorio = aleatorio(1,3)
+    let spaMonstruoEnemigo = document.getElementById('monstruo-enemigo')
+
+    if (monstruoAleatorio == 1) {
+        spaMonstruoEnemigo.innerHTML ='leviathan'
+    }else if (monstruoAleatorio == 2) {
+        spaMonstruoEnemigo.innerHTML ='Peligarroso'
+    }else {
+        spaMonstruoEnemigo.innerHTML ='Phairus'
+    }
+    }
+    
+function ataqueFuego (){
+    ataqueJugador ='Fuego'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua (){
+    ataqueJugador ='Agua'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra (){
+    ataqueJugador ='Tierra'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio =aleatorio(1,3)
+    
+    if (ataqueAleatorio == 1){
+        ataqueEnemigo = 'Fuego'
+    }else if (ataqueAleatorio == 2){
+        ataqueEnemigo ='Agua'
+    }else {
+        ataqueEnemigo ='Tierra'
+    }
+    
+}
+
+
+function aleatorio(min,max) {
+    return Math.floor(Math.random() * (max -min +1) +min)
 }
 
 window.addEventListener('load', iniciarJuego)
