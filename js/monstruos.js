a9242be95a78e2e6f270282321452413261be9fd
Maojg <mauriciojaramillog@gmail.com>
2023-02-11 10:11:18 -0500

    22

--- js/monstruos.js
+++ js/monstruos.js
@@ -1,15 +1,23 @@
-function iniciarJuego(){
-let botonMonstruoJugador = document.getElementById('boton-monstruo')
-botonMonstruoJugador.addEventListener('click', seleccionarMonstruoJugador)
+function iniciarJuego() {
+    let botonMonstruoJugador = document.getElementById('boton-monstruo')
+    botonMonstruoJugador.addEventListener('click', seleccionarMonstruoJugador)
 }
 
-function seleccionarMonstruoJugador(){
-    if(document.getElementById('gaiagua').checked) {
-        alert('seleccionaste a Gaiagua')
-    }
+function seleccionarMonstruoJugador() {
+    let inputLeviathan = document.getElementById('leviathan')
+    let inputPeligarroso = document.getElementById('peligarroso')
+    let inputPhairus = document.getElementById('phairus')
+    let spamMonstruoJugador = document.getElementById('monstruo-jugador')
+
+    if (inputLeviathan.checked) {
+        spamMonstruoJugador.innerHTML = 'Leviathan'
+    } else if (inputPeligarroso.checked) {
+        spamMonstruoJugador.innerHTML = 'Peligarroso'
+    } else if (inputPhairus.checked){
+        spamMonstruoJugador.innerHTML = 'Phairus'
+    }else {
+        alert('Selecciona un Monstruo')    
+    } 
 }
-
-
-
 
 window.addEventListener('load', iniciarJuego)
